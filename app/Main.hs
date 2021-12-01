dab1586969b88451cab9b7988ae768c063cc5d85
dfireBird <me@dfirebird.dev>
2021-12-01 09:22:34 +0530

    In the beginning, there was darkness

---
+++ app/Main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import           Control.Monad
+import           Data.Foldable
+import qualified Data.Map           as Map
+import qualified Day01              (solve)
+import           System.Environment
+import           System.Exit
+
+solutions = Map.fromList [("01", Day01.solve)]
+
+solveSingle :: String -> IO ()
+solveSingle s = case Map.lookup s solutions of
+    Just f  -> readFile (concat ["./data/day", s, ".txt"]) >>= f
+    Nothing -> putStrLn $ "Day not implemented: " ++ s
+
+solveProblems :: [String] -> IO ()
+solveProblems = mapM_ solveSingle
+
+main :: IO ()
+main = do
+    args <- getArgs
+    if null args
+        then solveProblems $ Map.keys solutions
+        else solveProblems args
