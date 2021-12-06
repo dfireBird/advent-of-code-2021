316d697ce0b4ce0bf66221ac93e43a9112cc8734
dfireBird <me@dfirebird.dev>
2021-12-06 19:37:16 +0530

    chore: refactor Day06 (into pointfree)

--- src/Day06.hs
+++ src/Day06.hs
@@ -8,11 +8,13 @@ import           Data.Maybe      (fromMaybe)
 parse :: String -> [Integer]
 parse = map read . splitOn ","
 
+toMap :: [Integer] -> Map Integer Integer
+toMap = foldr (\x map -> M.insertWith (+) x 1 map) M.empty
+
 simulate :: Map Integer Integer -> Map Integer Integer
-simulate fishes = if countZero == 0 then simulated else M.insertWith (+) 6 countZero $ M.insert 8 countZero simulated
+simulate fishes = M.insert 8 countZero . M.insertWith (+) 6 countZero . M.delete (-1) . M.mapKeys (+ (-1)) $ fishes
   where
     countZero = fromMaybe 0 $ M.lookup 0 fishes
-    simulated = M.delete (-1) . M.mapKeys (+ (-1)) $ fishes
 
 simulateForN :: Integer -> Map Integer Integer -> Map Integer Integer
 simulateForN n days = foldr (\_ b -> simulate b) days [1 .. n]
@@ -21,16 +23,10 @@ count :: Map Integer Integer -> Integer
 count = M.foldl (+) 0
 
 part1 :: String -> String
-part1 s = show . count . simulateForN 80 $ fishesDays
-  where
-    fishes = parse s
-    fishesDays = foldr (\x map -> M.insertWith (+) x 1 map) M.empty fishes
+part1 = show . count . simulateForN 80 . toMap . parse
 
 part2 :: String -> String
-part2 s = show . count . simulateForN 256 $ fishesDays
-  where
-    fishes = parse s
-    fishesDays = foldr (\x map -> M.insertWith (+) x 1 map) M.empty fishes
+part2 = show . count . simulateForN 256 . toMap . parse
 
 solve :: String -> IO ()
 solve input = putStrLn "--- Day 06 ---" >> putStrLn (part1 input) >> putStrLn (part2 input)
