f25ddff3ad1fee25acb99cf9c162ef7a6131a142
dfireBird <me@dfirebird.dev>
2021-12-07 11:21:27 +0530

    refactor: make the second part of solution go brrrrr

--- src/Day07.hs
+++ src/Day07.hs
@@ -17,7 +17,7 @@ part2 s = show $ minimum $ map (helper positions) [0 .. maximum positions]
   where
     positions = parse s
 
-    helper xs n = sum . map (\x -> sum [1 .. abs (n - x)]) $ xs
+    helper xs n = sum . map (\x -> let c = abs (n - x) in (c * (c + 1)) `div` 2) $ xs
 
 solve :: String -> IO ()
 solve input = putStrLn "--- Day 07 ---" >> putStrLn (part1 input) >> putStrLn (part2 input)
